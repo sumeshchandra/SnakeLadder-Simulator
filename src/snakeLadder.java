5e41d1750774075a3568bdfb5137454a23b650cb
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-13 23:36:42 +0530

    Added: Roll a die to get a no

--- src/snakeLadder.java
+++ src/snakeLadder.java
@@ -4,6 +4,9 @@ public class snakeLadder {
 	static int initialPosition = 0;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake Ladder Game");
-			 System.out.println("snake&ladder start with single player at start pos : " + initialPosition);
+			System.out.println("snake&ladder start with single player at start pos : " + initialPosition);
+
+		 int rollDice =(int) Math.floor(Math.random()*6+1);
+			System.out.println("player roll a dice: " + rollDice);
 		}
 }
