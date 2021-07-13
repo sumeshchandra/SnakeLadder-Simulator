713dfcf5757a6504d1487ce64eafc36609910c27
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-07-13 23:29:20 +0530

    Added: Single position 0

--- src/snakeLadder.java
+++ src/snakeLadder.java
@@ -3,7 +3,7 @@ public class snakeLadder {
 
 	static int initialPosition = 0;
 	public static void main(String[] args) {
-			System.out.println("Welcome to Snake Ladder Game");
+		System.out.println("Welcome to Snake Ladder Game");
 			 System.out.println("snake&ladder start with single player at start pos : " + initialPosition);
 		}
 }
